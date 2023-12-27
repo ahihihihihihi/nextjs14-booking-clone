dc18a7ada3e17f0fa7e6817b68d21fc1c1b3bd9a
ahihi <bdsthocu20@gmail.com>
2023-12-27 17:52:14 +0700

    #3

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "/688893.jpg",
+        title: "Dubai",
+        location: "United Arab Emirates",
+        description: "15 Deals",
+    },
+
+    {
+        id: 2,
+        src: "/688844.jpg",
+        title: "South Korea",
+        location: "Asia",
+        description: "32 Deals"
+    },
+    {
+        id: 3,
+        src: "/688853.jpg",
+        title: "London",
+        location: "United Kingdom", description: "45 Deals"
+    },
+    {
+        id: 4,
+        src: "/688831.jpg",
+        title: "Singapore",
+        location: "Asia",
+        description: "88 Deals"
+    },
+    {
+        id: 5,
+        src: "/688956.jpg",
+        title: "New York City",
+        location: "United States of America", description: "92 Deals"
+    },
+
+]
